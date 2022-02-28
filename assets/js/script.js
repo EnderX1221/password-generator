25abd08efe1250cdba144ce3f75513d99ad658c6
Javier Razo <javier.razo1221@gmail.com>
2022-02-27 23:17:57 -0600

    finished password

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,42 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var getRandomNmber = ["1","2","3","4","5"];
+var getRandomUpper = ["A", "B", "C", "D"];
+var getRandomLower= [ "a", "b", "c", "d"];
+var getRandomSymbol= [ "#", "*", "$", "^", "%"];
 
+
+//Prompt user for password criteria 8-128 characters, uppercase, lowercase, symbols
+function generatePassword() {
+  console.log("Button was pushed by user")
+  window.prompt("How many characters do you want your passwrod to be?");
+
+  function getRandomNmber() {
+return getRandomNmber("1","2","3","4","5);
+  }
+
+  function getRandomLower() {
+    return getRandomLower("a", "b", "c", "d");
+  }
+function getRandomSymbol() {
+  return getRandomSymbol("#", "*", "$", "^", "%);
+}
+funtion getRandomUpper() {
+  return getRandomUpper("A", "B", "C", "D");
+}
+window.prompt("Do you watn upper and lower?");
+
+
+window.prompt("Do you want symbols?")
+
+
+
+
+//Dsiplay password
+  return "password generated";
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
